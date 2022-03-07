75d8c18c6d68b9d024e61af58f31b15a23c6251b
Allen <coryroberts5699@gmail.com>
2022-03-07 14:51:13 -0500

    Fixed http issue

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchIcon from "./search.svg";
 import "./App.css";
 import MovieCard from "./MovieCard";
 
-const API_URL = "http://www.omdbapi.com?apikey=a08aa36c";
+const API_URL = "https://www.omdbapi.com?apikey=a08aa36c";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
